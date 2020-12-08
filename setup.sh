184794fbbd1d6e9846d7a4540290066658939159
Simon Will <will@cl.uni-heidelberg.de>
2020-12-08 18:02:48 +0100

    Fix some setup issues

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ export FLASK_DEBUG=true
 THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 POTENTIAL_VENV="$THIS_DIR/venv"
-if [ -z "$VIRTUAL_ENV" ] && [ -d "$POTENTIAL_VENV" ]; then
+if [ -z "$VIRTUAL_ENV" ] && [ -z "$CONDA_DEFAULT_ENV" ] \
+	&& [ -d "$POTENTIAL_VENV" ]; then
     . "$POTENTIAL_VENV/bin/activate"
 fi
 unset POTENTIAL_VENV
