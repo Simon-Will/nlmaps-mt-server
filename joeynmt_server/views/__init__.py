853331956b84108d5b0d9a7526b043cd9ec682fb
Simon Will <will@cl.uni-heidelberg.de>
2021-02-13 13:57:35 +0100

    Improve error handling

--- joeynmt_server/views/__init__.py
+++ joeynmt_server/views/__init__.py
@@ -1,3 +1,4 @@
+from .errors import page_forbidden, page_not_found, internal_server_error
 from .feedback import (get_feedback, edit_feedback, query_feedback,
                        save_feedback)
 from .translate import translate
---
+++ joeynmt_server/views/errors.py
@@ -0,0 +1,28 @@
+import traceback
+
+from flask import current_app, jsonify, render_template, request
+from flask_login import current_user
+
+
+@current_app.errorhandler(403)
+def page_forbidden(e):
+    current_app.logger.warning('Error 403. URL {}. {}'
+                               .format(request.url, e))
+    return jsonify({'error': 'Forbidden'}), 403
+
+
+@current_app.errorhandler(404)
+def page_not_found(e):
+    current_app.logger.warning('Error 404. URL {}. {}'
+                               .format(request.url, e))
+    return jsonify({'error': 'Not Found'}), 404
+
+
+@current_app.errorhandler(Exception)
+def internal_server_error(e):
+    trace = traceback.format_exc()
+    current_app.logger.error(
+        'Error 500. URL {}. {}'
+        .format(request.url, trace)
+    )
+    return jsonify({'error': 'Internal Server Error'}), 500
